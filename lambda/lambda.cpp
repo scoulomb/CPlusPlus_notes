6c0abaa1ba713f2c5574780daef3a7a529162f75
Sylvain Coulombel <sylvaincoulombel@gmail.com>
2016-05-18 18:38:36 +0200

    Add a find with a functor

--- lambda/lambda.cpp
+++ lambda/lambda.cpp
@@ -49,7 +49,7 @@ std::ostream& operator<<(std::ostream& os, Guest& g)
 std::ostream& operator<<(std::ostream& os, HotelBooking b)
 {
   os << "Reservation has confirmationNumber " << b.confirmationNumber << std::endl;
-  os << "Guest are:" << std::endl;
+  os << "Guests are:" << std::endl;
   for_each(b.guests.begin(), b.guests.end(),
                                            [&] (mdw::bom::Guest& g)
                                            {
@@ -62,18 +62,58 @@ std::ostream& operator<<(std::ostream& os, HotelBooking b)
 }
 }
 
+namespace mdw
+{
+namespace tools
+{
+
+class IdComparator {
+public:
+  IdComparator(int iGuestIdToFind):_guestIdToFind(iGuestIdToFind) {};
+public:
+bool operator()(mdw::bom::Guest& iGuestToTest) {return iGuestToTest.id == _guestIdToFind;}
+
+private:
+  int _guestIdToFind;
+};
+
+}
+}
+
+namespace helper
+{
+
+void fillABooking(mdw::bom::HotelBooking& iHotelBooking) // ref also here, otherwise same as below updqte a copy of ref
+{
+  std::cout << "fillABooking begin" << std::endl;
+
+  // not auto guest&
+  auto& guests = iHotelBooking.guests; // ref is important here
+                                       // if in main directly: otherwise guests vector inside hotelbooking is not updated but the local value! (see cout)
+                                       // if in function as here: even worse because we are updating a copy of a ref -> core dump
+
+  guests.push_back(mdw::bom::Guest(1, "coulombel", "sylvain"));
+  guests.push_back(mdw::bom::Guest(2, "sarkozy", "nicolas"));
+  guests.push_back(mdw::bom::Guest(3, "hollande", "francois"));
+  guests.push_back(mdw::bom::Guest(4, "chirac", "jacques"));
+
+  std::cout << iHotelBooking;
+  std::cout << "fillABooking end" << std::endl;
+}
+
+}
+
 
 int main()
 {
-   mdw::bom::HotelBooking hotelBooking("12345678");
-   auto& guests = hotelBooking.guests; // ref is important here
-                                       // otherwise guests vector inside hotelbooking is not updated but the local value! (see cout)
+  mdw::bom::HotelBooking hotelBooking("12345678");
+  helper::fillABooking(hotelBooking);
 
-   guests.push_back(mdw::bom::Guest(1, "coulombel", "sylvain"));
-   guests.push_back(mdw::bom::Guest(2, "sarkozy", "nicolas"));
-   guests.push_back(mdw::bom::Guest(3, "hollande", "francois"));
-   guests.push_back(mdw::bom::Guest(4, "chirac", "jacques"));
+   ////// JPR p46
+   // find_if(InputIterator first, InputIterator last, UnaryPredicate pred)
 
-   std::cout << hotelBooking;
+   auto res = find_if(hotelBooking.guests.begin(), hotelBooking.guests.end(), mdw::tools::IdComparator(3));
+   std::cout << *res;
 
+   
 }
