22934614e83e3bfc5bc06dff3c7bfdcd6eb0401a
Sylvain Coulombel <sylvaincoulombel@gmail.com>
2016-05-29 02:42:55 +0200

    Make run script fail on error

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+set -e
+# Any subsequent(*) commands which fail will cause the shell script to exit immediately
+
 # Generate top level SConstruct file
 find . -mindepth 2 -name "SConstruct" | python generateSConstruct.py
 
